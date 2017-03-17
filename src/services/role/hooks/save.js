3714a4cca07bda984d9a88064c9c75e753b5e084
Alvin Chan <aschan@gmail.com>
2017-03-17 16:54:12 -0700

    Check if `roles` exist before attempting to save

--- src/services/role/hooks/save.js
+++ src/services/role/hooks/save.js
@@ -4,21 +4,26 @@ exports.default = function (options) {
   return async function (hook) {
     const sequelize = hook.app.get('sequelize')
     const { roles: Role } = sequelize.models
+    const roles = hook.data.roles
     const model = hook.result
 
-    const roles = await Role.findAll({
+    if (!roles) {
+      return
+    }
+
+    const rolesFound = await Role.findAll({
       where: {
         name: {
-          $in: hook.data.roles
+          $in: roles
         }
       },
       through: { attributes: [] }
     })
 
     // Save the associations
-    await model.setRoles(roles)
+    await model.setRoles(rolesFound)
 
     // Update the response
-    model.set('roles', roles)
+    model.set('roles', rolesFound)
   }
 }
