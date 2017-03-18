d162dd6e33fd1808656cf341b3070cb4d78fe299
Alvin Chan <aschan@gmail.com>
2017-03-18 00:58:40 -0700

    Replace PostgreSQL with SQLite for portability

--- src/services/index.js
+++ src/services/index.js
@@ -2,22 +2,25 @@
 
 // Dependencies
 const Sequelize = require('sequelize')
+
+// Services
 const authentication = require('./authentication')
 const post = require('./post')
 const role = require('./role')
 const tag = require('./tag')
 const user = require('./user')
 
-// Services
 module.exports = function () {
   const app = this
 
   // Connect to the database
-  const sequelize = new Sequelize(app.get('postgres'), {
-    dialect: 'postgres',
+  const database = app.get('database')
+  const sequelize = new Sequelize(database, {
+    dialect: database.split(':')[0],
     logging: false
   })
 
+  // Attach Sequelize to the application
   app.set('sequelize', sequelize)
 
   // Register the services
